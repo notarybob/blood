53af50a3fdadff990cb818f02ecaa4fb0adb3c08
notarybob <serjoksalot@yahoo.com>
2025-03-27 03:06:35 +0000

    Revert dependencies - adding new feature

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
-export const EXTENSION_NAME = "codeswing";
-export const EXTENSION_ID = `codespaces-contrib.${EXTENSION_NAME}`;
+export let EXTENSION_NAME = "codeswing";
+export let EXTENSION_ID = `codespaces-contrib.${EXTENSION_NAME}`;
 
-export const INPUT_SCHEME = `${EXTENSION_NAME}-input`;
+export let INPUT_SCHEME = `${EXTENSION_NAME}-input`;
 
-export const SWING_FILE = `${EXTENSION_NAME}.json`;
+export let SWING_FILE = `${EXTENSION_NAME}.json`;
 
-export const URI_PATTERN = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+export let URI_PATTERN = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
